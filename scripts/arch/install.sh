aa9ee306ce56e9c3167e171f53ee178d3122c541
nakamura0907 <nakamura990907@gmail.com>
2023-11-24 12:08:58 +0900

    Arch Linux用インストールスクリプト仮の作成

---
+++ scripts/arch/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$(realpath "$SCRIPT_DIR/../../")"
+
+# パッケージのインストール
+
+echo ""
+echo "========================================"
+echo "Installing packages..."
+echo "========================================"
+echo ""
+
+sudo pacman -S --needed \
+    neovim
+
+# 設定ファイルの配置
+
+echo ""
+echo "========================================"
+echo "Creating symbolic links..."
+echo "========================================"
+echo ""
+
+for f in $PROJECT_ROOT/.config/*; do
+    ln -snfv $f ~/.config/
+done
