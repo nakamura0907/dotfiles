e2d406a6eb07255166e78c2d244631733fc0d6cc
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-24 17:56:50 +0900

    コードのフォーマット

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,25 +1,29 @@
 import XMonad
-import XMonad.Hooks.ManageDocks 
-import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.SpawnOnce (spawnOnce)
 
 main :: IO ()
-main = xmonad $ ewmh $ docks $ def
- {
-  startupHook = spawnOnce "~/.config/polybar/launch.sh &"
-  , manageHook = myManageHook <+> manageHook def
-  , layoutHook =  avoidStruts $ layoutHook def
-  , terminal = myTerminal
-  , modMask = mod4Mask
- } `additionalKeysP` myKeys
+main =
+  xmonad $
+    ewmh $
+      docks $
+        def
+          { startupHook = spawnOnce "~/.config/polybar/launch.sh &",
+            manageHook = myManageHook <+> manageHook def,
+            layoutHook = avoidStruts $ layoutHook def,
+            terminal = myTerminal,
+            modMask = mod4Mask
+          }
+          `additionalKeysP` myKeys
 
 ------------------------
 -- Terminal
 ------------------------
- 
+
 myTerminal = "alacritty"
 
 ------------------------
@@ -27,15 +31,16 @@ myTerminal = "alacritty"
 ------------------------
 
 myManageHook :: ManageHook
-myManageHook = composeAll
- [ className =? "Stalonetray" --> doFloat
- ]
+myManageHook =
+  composeAll
+    [ className =? "Stalonetray" --> doFloat
+    ]
 
 ------------------------
 -- Key bindings
 ------------------------
 
-myKeys =  [
-  ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-  , ("M1-S-l", spawn "i3lock-fancy")
- ]
+myKeys =
+  [ ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
+    ("M1-S-l", spawn "i3lock-fancy")
+  ]
