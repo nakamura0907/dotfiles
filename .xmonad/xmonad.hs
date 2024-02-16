bd47a9086d05e80086db9978a2afd2d35c45defa
nakamura0907 <nakamura0907@users.noreply.github.com>
2024-02-16 21:21:12 +0900

    XMonad: move help document to comment

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,65 +8,79 @@ import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Layout.Spacing
 
 ------------------------
--- Terminal
+-- General Settings
 ------------------------
 
+-- The preferred terminal application. Default: "xterm"
+--
 myTerminal :: String
 myTerminal = "alacritty"
 
+-- the mod modifier
+-- mod4Mask is the windows key
+--
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+-- The border width
+--
+myBorderWidth :: Dimension
+myBorderWidth = 2
+
 ------------------------
--- layoutHook
+-- Keybindings
+------------------------
+
+myKeys :: [(String, X ())]
+myKeys =
+  [ 
+    ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
+    ("M-S-r", spawn "xmonad --recompile; xmonad --restart"),
+    ("M1-S-l", spawn "i3lock-fancy")
+  ]
+
+------------------------
+-- Layouts
 ------------------------
 
 myLayoutHook = avoidStruts $ spacing 2 $ layoutHook def
 
 ------------------------
--- ManageHook
+-- Window Rules
 ------------------------
 
+-- ManageHook
+--
+-- To find the class name of a window, use `xprop | grep WM_CLASS`
+-- and click on the window.
+--
+-- When using appName, pass the first value of WM_CLASS.
+-- For className, pass the second value.
+--
+-- If WM_CLASS cannot be obtained, use title to match WM_NAME
+--
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ 
+    [
       className =? "Stalonetray" --> doFloat,
-      className =? "zoom" --> doFloat
+      className =? "zoom" --> doFloat,
+      -- Brave-browser Picture in Picture
+      title =? "ピクチャー イン ピクチャー" --> doFloat
     ]
 
-------------------------
--- modMask
-------------------------
 
-myModMask :: KeyMask
-myModMask = mod4Mask
 
 ------------------------
--- borderWidth
-------------------------
-
-myBorderWidth :: Dimension
-myBorderWidth = 2
-
-------------------------
--- startupHook
+-- Startup Hook
 ------------------------
 
 myStartupHook :: X ()
--- myStartupHook = spawnOnce "~/.config/polybar/launch.sh &"
 myStartupHook = do
   spawnOnce "~/.config/polybar/launch.sh &"
   spawnOnce "picom --vsync -b"
 
 ------------------------
--- Key bindings
-------------------------
-
-myKeys =
-  [ 
-    ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
-    ("M1-S-l", spawn "i3lock-fancy")
-  ]
-
-------------------------
--- Main
+-- Run XMonad
 ------------------------
 
 main :: IO ()
